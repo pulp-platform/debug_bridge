pulp-platform/debug_bridge

commit 21eb86c7fe4795a09ff3aa1cb5089b503b55004f

User proper conversion specifiers as per inttypes.h

rsp.cpp
@@ -1,4 +1,4 @@
-
+#include <inttypes.h>
 #include "rsp.h"
 
 enum mp_type {
@@ -196,10 +196,10 @@ Rsp::cont(char* data, size_t len) {
 
   // strip signal first
   if (data[0] == 'C') {
-    if (sscanf(data, "C%X;%X", &sig, &addr) == 2)
+    if (sscanf(data, "C%" SCNx32 ";%" SCNx32 "", &sig, &addr) == 2)
       npc_found = true;
   } else {
-    if (sscanf(data, "c%X", &addr) == 1)
+    if (sscanf(data, "c%" SCNx32 "", &addr) == 1)
       npc_found = true;
   }
 
@@ -233,7 +233,7 @@ Rsp::step(char* data, size_t len) {
     }
   }
 
-  if (sscanf(data, "%x", &addr) == 1) {
+  if (sscanf(data, "%" SCNx32, &addr) == 1) {
     dbgif = this->get_dbgif(m_thread_sel);
     // only when we have received an address
     dbgif->read(DBG_NPC_REG, &npc);
@@ -306,7 +306,7 @@ Rsp::query(char* data, size_t len) {
     const char* str_default = "Unknown Core";
     char str[256];
     unsigned int thread_id;
-    if (sscanf(data, "qThreadExtraInfo,%d", &thread_id) != 1) {
+    if (sscanf(data, "qThreadExtraInfo,%u", &thread_id) != 1) {
       fprintf(stderr, "Could not parse qThreadExtraInfo packet\n");
       return this->send_str("");
     }
@@ -440,7 +440,7 @@ Rsp::reg_read(char* data, size_t len) {
   uint32_t rdata;
   char data_str[10];
 
-  if (sscanf(data, "%x", &addr) != 1) {
+  if (sscanf(data, "%" SCNx32, &addr) != 1) {
     fprintf(stderr, "Could not parse packet\n");
     return false;
   }
@@ -464,7 +464,7 @@ Rsp::reg_write(char* data, size_t len) {
   uint32_t wdata;
   DbgIF* dbgif;
 
-  if (sscanf(data, "%x=%08x", &addr, &wdata) != 2) {
+  if (sscanf(data, "%" SCNx32 "=%08" SCNx32, &addr, &wdata) != 2) {
     fprintf(stderr, "Could not parse packet\n");
     return false;
   }
@@ -938,7 +938,7 @@ Rsp::mem_read(char* data, size_t len) {
   unsigned int length;
   uint32_t rdata;
 
-  if (sscanf(data, "%x,%x", &addr, &length) != 2) {
+  if (sscanf(data, "%" SCNx32 ",%" SCNx32, &addr, &length) != 2) {
     fprintf(stderr, "Could not parse packet\n");
     return false;
   }
@@ -963,7 +963,7 @@ Rsp::mem_write_ascii(char* data, size_t len) {
   char* buffer;
   int buffer_len;
 
-  if (sscanf(data, "%x,%ld:", &addr, &length) != 2) {
+  if (sscanf(data, "%" SCNx32 ",%zd:", &addr, &length) != 2) {
     fprintf(stderr, "Could not parse packet\n");
     return false;
   }
@@ -1019,7 +1019,7 @@ Rsp::mem_write(char* data, size_t len) {
   size_t length;
   unsigned int i;
 
-  if (sscanf(data, "%x,%lx:", &addr, &length) != 2) {
+  if (sscanf(data, "%" SCNx32 ",%zd:", &addr, &length) != 2) {
     fprintf(stderr, "Could not parse packet\n");
     return false;
   }
@@ -1048,7 +1048,7 @@ Rsp::bp_insert(char* data, size_t len) {
   uint32_t addr;
   int bp_len;
 
-  if (3 != sscanf(data, "Z%1d,%x,%1d", (int *)&type, &addr, &bp_len)) {
+  if (3 != sscanf(data, "Z%1d,%" SCNx32 ",%1d", (int *)&type, &addr, &bp_len)) {
     fprintf(stderr, "Could not get three arguments\n");
     return false;
   }

commit 2b923cf32de546a65ae2178a3aa082dbe5bb3b48

rsp: fix wrong conversion specifiers
The values sent by gdb in memory writes are _hexa_decimal.
The lengths read into size_t variables are unsigned.
The wrong specifiers were introduced by myself in 21eb86c7.

rsp.cpp
@@ -1124,7 +1124,7 @@ Rsp::mem_write_ascii(char* data, size_t len) {
   char* buffer;
   int buffer_len;
 
-  if (sscanf(data, "%" SCNx32 ",%zd:", &addr, &length) != 2) {
+  if (sscanf(data, "%" SCNx32 ",%zu:", &addr, &length) != 2) {
     fprintf(stderr, "Could not parse packet\n");
     return false;
   }
@@ -1180,7 +1180,7 @@ Rsp::mem_write(char* data, size_t len) {
   size_t length;
   unsigned int i;
 
-  if (sscanf(data, "%" SCNx32 ",%zd:", &addr, &length) != 2) {
+  if (sscanf(data, "%" SCNx32 ",%zx:", &addr, &length) != 2) {
     fprintf(stderr, "Could not parse packet\n");
     return false;
   }
@@ -1276,7 +1276,7 @@ Rsp::encode_hex(const char *in, char *out, size_t out_len) {
   // This check guarantees that for every non-\0 character in the input 
   // there are two bytes available in the out buffer + one for trailing \0.
   if (2*in_len - 1 > out_len) {
-    fprintf(stderr, "%s: output buffer too small (need %zd, have %zd)\n", 
+    fprintf(stderr, "%s: output buffer too small (need %zu, have %zu)\n", 
       __func__, 2*in_len - 1, out_len);
     return false;
   }